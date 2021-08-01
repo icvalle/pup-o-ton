10c5c48d0f37a47731cbaec01511ae12e6597d6f
ryanascherr <ryanascherr@gmail.com>
2021-08-01 12:21:25 -0400

    Added color and other styling changes throughout application.

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,9 +3,9 @@ import React from 'react';
 
 function Footer() {
     return (
-        <div className="footer">
-            <h1>Footer</h1>
-        </div>
+        <footer className="footer">
+            <h4>Made with love by <a href="https://github.com/tylerf808" target="_blank">Tyler</a>, <a href="https://github.com/icvalle" target="_blank">Iris</a>, <a href="https://github.com/caitbopp" target="_blank">Caitlin</a>, and <a href="https://github.com/ryanascherr" target="_blank">Ryan</a></h4>
+        </footer>
     )
 }
 
