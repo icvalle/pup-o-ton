c0376eb2872202b97519b49b57b991351a22b29a
Iris Valle <iris.valle.design@gmail.com>
2021-08-03 01:02:43 -0400

    updating schemas

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,18 +2,40 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
     type User {
-        _id: ID!
-        username: String!
+        _id: ID
+        username: String
         email: String
         password: String
     }
 
     type Dog {
-        _id: ID!
+        _id: ID
         name: String!
         age: Int
         breed: String
         weight: Int
+        image: : String
+    }
+
+    type UserDog {
+        _id: ID
+        userId: [User]
+        dogs: [Dog]
+    }
+
+    type DogExercise {
+        _id: ID
+        dogId: [Dog]
+        dogs: [Exercise]
+    }
+
+    type Exercise {
+        _id: ID
+        day: Date
+        type: String
+        name: String
+        duration: Int
+        sets: Int
     }
 
     type Auth {
@@ -22,15 +44,24 @@ const typeDefs = gql`
     }
 
     type Query {
-        user: User,
-        dogs: [Dog],
+        users: [User]
+        user(username: String!): User
+        userDog(userId: ID!): Dog
+        getDog: Dog
+        dogExercise(dogId: ID!): Exercise
+        getExercise: Exercise
+        dogs: [Dog]
+        exercises: [Exercise]
+        me: User
     }
 
     type Mutation {
-        addUser(username: String!, email: String!, password: String!): Auth,
-        login(email: String!, password: String!): Auth,
-        createDog(name: String!, age: Int, breed: String, weight: Int): Dog,
-        
+        addUser(username: String!, email: String!, password: String!): Auth
+        login(email: String!, password: String!): Auth
+        addDog(name: String!, age: Int, breed: String, weight: Int, image: String): Dog
+        addUserDog(name: String!, age: Int, breed: String, weight: Int, image: String): Dog
+        addDogExercise(day: Date, type: String!, name: String!, duration: Int!, sets: Int!): Exercise
+        addExercise(day: Date, type: String!, name: String!, duration: Int!, sets: Int!): Exercise
     }
 `;
 
